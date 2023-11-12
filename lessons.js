cb64d810e4e5f40bec5e443182c01212f954548c
Nibras <nibras.sid@gmail.com>
2023-11-12 02:43:13 +0000

    coursework

---
+++ lessons.js
@@ -0,0 +1,99 @@
+let lessons = [
+    {
+            id: 1001,
+            title: "Chemistry",
+            description: "This is Chemistry",
+            location: "Startford",
+            price: 9000,
+            image: "images/chemistry.png",
+            availableInventory: 5,
+        },
+
+        {
+            id: 1002,
+            title: "Maths",
+            description: "This is Maths Lesson",
+            location: "Whitechapel",
+            price: 3000,
+            image: "images/maths.png",
+            availableInventory: 7,
+        },
+
+        {
+            id: 1003,
+            title: "Physics",
+            description: "This is Physics Lesson",
+            location: "Barking",
+            price: 4000,
+            image: "images/physics.png",
+            availableInventory: 9,
+        },
+
+        {
+            id: 1004,
+            title: "English",
+            description: "This is English Lesson",
+            location: "Henden",
+            price: 5000,
+            image: "images/english.png",
+            availableInventory: 5,
+        },
+
+        {
+            id: 1005,
+            title: "Biology",
+            description: "This is Biology Lesson",
+            location: "Liverpool Street",
+            price: 6000,
+            image: "images/biology.png",
+            availableInventory: 5,
+        },
+
+        {
+            id: 1006,
+            title: "Computer",
+            description: "This is Computer Lesson",
+            location: "Ilford",
+            price: 7000,
+            image: "images/computer.png",
+            availableInventory: 5,
+
+
+        },
+        {
+            id: 1007,
+            title: "UX Design",
+            description: "This is UX Design Lesson",
+            location: "Luton",
+            price: 3000,
+            image: "images/computer.png",
+            availableInventory: 5,
+        },
+        {
+            id: 1008,
+            title: "Painting",
+            description: "This is a Painting Lesson",
+            location: "Oxford Street",
+            price: 4000,
+            image: "images/computer.png",
+            availableInventory: 8,
+        },
+        {
+            id: 1009,
+            title: "Airtificial Intelligence",
+            description: "This is a AI Lesson",
+            location: "Romford",
+            price: 3000,
+            image: "images/computer.png",
+            availableInventory: 5,
+        },
+        {
+            id: 1010,
+            title: "Web-Based Mobile App",
+            description: "This is Mobile App Lesson",
+            location: "SevenKings",
+            price: 3000,
+            image: "images/computer.png",
+            availableInventory: 5,
+        },
+    ]
